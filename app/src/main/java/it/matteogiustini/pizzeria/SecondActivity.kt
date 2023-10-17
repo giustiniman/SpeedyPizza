dba48a8fc56bca32882909d555ec6f7ea7f50811
giustiniman <justointer@gmail.com>
2023-10-17 10:32:20 +0200

    primo commit

---
+++ app/src/main/java/it/matteogiustini/pizzeria/SecondActivity.kt
@@ -0,0 +1,11 @@
+package it.matteogiustini.pizzeria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu)
+    }
+}
